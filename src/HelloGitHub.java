8fb59f91ab6e285da2b56c1dd324a9cabbfc9c44
snowflying233 <2936092243@qq.com>
2020-12-29 21:57:13 +0800

    update AAA

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,5 +3,6 @@ public class HelloGitHub
 	public static void main(String[] args) {
 		System.out.println("hello GitHub!");
 		System.out.println("from xjl-flying update!");
+		System.out.println("AAA");
 	}
 }
